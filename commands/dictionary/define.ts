d4e105e828e33b8fa0edefca7069d6f47c2c14e7
manas <bagrimanasbir@gmail.com>
2024-08-03 17:37:43 -0400

    fix dict

--- commands/dictionary/define.ts
+++ commands/dictionary/define.ts
@@ -1,17 +1,15 @@
 const { ChatInputCommandInteraction, SlashCommandBuilder } = require("discord.js");
-const Dictionary = require("../../dictionary.ts");
-
+import Dictionary from "@/dictionary"
 const dictionary = new Dictionary(process.env.DICTIONARY_API_KEY!);
 
-module.exports = {
-    data: new SlashCommandBuilder()
+export const data = new SlashCommandBuilder()
 		.setName('define')
 		.setDescription('defines a word')
 		.addStringOption(option =>
 			option.setName('word')
-				.setDescription('word to define')),
-		async execute(interaction) {
+				.setDescription('word to define'))
+
+export async function execute(interaction) {
 			let res = await dictionary.get_info(interaction.options.getString('word')!)
 			await interaction.reply(res ? res : "an error occurred");
 		}
-};
